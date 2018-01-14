107efe65f083757df91ec994706acc1663cf7c8b
Marvin Schulz <schulzma@googlemail.com>
2018-01-14 09:25:23 +0100

    Following the tutorial

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,41 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <h1> Welcome to CobraOnline </h1>
+    <h4> Organism </h4>
+    <input v-model="organism_input"> </input>
+    <button v-on:click="updateOrganism">Press me</button>
+    <div v-if="show_result">
+      Result: {{ selected_organism }}
+      <ol>
+        <li v-for="organism in organisms"> {{ organism.name }} </li>
+      </ol>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+    name: 'App',
+    data() {
+	return {
+	    message: 'Hello World',
+	    show_result: true,
+	    organism_input: '',
+	    selected_organism: '',
+	    organisms: [
+	        { name: 'Saccharomyces cerevisiae', value: 'iSH654' },
+	        { name: 'Escherichia coli', value: 'iAD123' }
+	    ]
+	}
+    },
+    methods: {
+        updateOrganism: function(){
+	    this.selected_organism = this.organism_input;
+	    this.show_result = !this.show_result;
+	}
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
